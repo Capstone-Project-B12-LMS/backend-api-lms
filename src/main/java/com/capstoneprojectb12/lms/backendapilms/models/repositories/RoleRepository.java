9d32916c4a0f036218dd7b8bc3aa37d3ede885c4
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-06-03 23:21:59 +0700

    create pageable derived query method

--- src/main/java/com/capstoneprojectb12/lms/backendapilms/models/repositories/RoleRepository.java
+++ src/main/java/com/capstoneprojectb12/lms/backendapilms/models/repositories/RoleRepository.java
@@ -2,6 +2,8 @@ package com.capstoneprojectb12.lms.backendapilms.models.repositories;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,6 @@ import com.capstoneprojectb12.lms.backendapilms.models.entities.Role;
 @Repository
 public interface RoleRepository extends JpaRepository<Role, String> {
     public Optional<Role> findByNameEqualsIgnoreCase(String name);
+
+    public Page<Role> findAll(Pageable pageable);
 }
