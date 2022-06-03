222569006411e397eecc1d370b1cd46339f68fb8
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-06-04 02:43:37 +0700

    add base query

--- src/main/java/com/capstoneprojectb12/lms/backendapilms/controllers/gql/role/RoleQuery.java
+++ src/main/java/com/capstoneprojectb12/lms/backendapilms/controllers/gql/role/RoleQuery.java
@@ -6,6 +6,7 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
+import com.capstoneprojectb12.lms.backendapilms.controllers.gql.base.BaseQuery;
 import com.capstoneprojectb12.lms.backendapilms.models.entities.Role;
 import com.capstoneprojectb12.lms.backendapilms.services.RoleService;
 import com.capstoneprojectb12.lms.backendapilms.utilities.gql.PaginationResponse;
@@ -15,16 +16,18 @@ import lombok.RequiredArgsConstructor;
 @Controller
 @SchemaMapping(typeName = "RoleQuery")
 @RequiredArgsConstructor
-public class RoleQuery {
+public class RoleQuery implements BaseQuery<Role> {
     private final RoleService roleService;
 
+    @Override
     @SchemaMapping(field = "findAll")
     public List<Role> findAll() {
         return this.roleService.findAll();
     }
 
+    @Override
     @SchemaMapping(field = "findAllWithPageable")
-    public PaginationResponse<List<Role>> findAll(
+    public PaginationResponse<List<Role>> findAllWithPageable(
             @Argument(name = "page") int page,
             @Argument(name = "size") int size) {
 
@@ -32,4 +35,15 @@ public class RoleQuery {
         var response = this.roleService.toPaginationResponse(pageRoles);
         return response;
     }
+
+    @Override
+    public List<Role> findAllDeleted() {
+        return null;
+    }
+
+    @Override
+    public PaginationResponse<List<Role>> findAllDeletedWithPageable(int page, int size) {
+        return null;
+    }
+
 }
