f80ddc99e0f10444bd83c327ade699d27c0c5e93
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-06-03 21:43:49 +0700

    initialize Rest FULL API controller

--- src/main/java/com/capstoneprojectb12/lms/backendapilms/controllers/rest/InitRestFullAPI.java
+++ src/main/java/com/capstoneprojectb12/lms/backendapilms/controllers/rest/InitRestFullAPI.java
@@ -1,10 +1,36 @@
 package com.capstoneprojectb12.lms.backendapilms.controllers.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import com.capstoneprojectb12.lms.backendapilms.entities.responses.SayHelloInput;
+import com.capstoneprojectb12.lms.backendapilms.entities.responses.SayHelloResponse;
+import com.capstoneprojectb12.lms.backendapilms.utilities.ApiValidation;
 
 @RestController
 @RequestMapping(value = { "/restapi" })
 public class InitRestFullAPI {
+    @GetMapping
+    public ResponseEntity<?> init() {
+        var response = SayHelloResponse.builder()
+                .message("Hi, my name is KELOMPOK B 12")
+                .build();
+        return ResponseEntity.ok(response);
+    }
 
+    @PostMapping
+    public ResponseEntity<?> init(@RequestBody @Valid SayHelloInput to, Errors errors) {
+        if (errors.hasErrors()) {
+            return ResponseEntity.badRequest().body(ApiValidation.getErrorMessages(errors));
+        }
+
+        var response = SayHelloResponse
+                .builder()
+                .message("Hello " + to.getName().toUpperCase() + " how are you?")
+                .build();
+        return ResponseEntity.ok(response);
+    }
 }
