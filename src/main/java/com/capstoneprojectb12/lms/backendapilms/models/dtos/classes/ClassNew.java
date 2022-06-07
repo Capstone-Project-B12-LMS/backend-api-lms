7c18204f83a2927816e2a0d1a485d18151a8702c
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-06-07 23:39:23 +0700

    create schema ClassUpdate

---
+++ src/main/java/com/capstoneprojectb12/lms/backendapilms/models/dtos/classes/ClassNew.java
@@ -0,0 +1,22 @@
+package com.capstoneprojectb12.lms.backendapilms.models.dtos.classes;
+
+import javax.validation.constraints.*;
+
+import com.capstoneprojectb12.lms.backendapilms.models.entities.utils.ClassStatus;
+
+public class ClassNew {
+
+    @NotNull(message = "cannot be null")
+    @NotBlank(message = "cannot be blank")
+    @NotEmpty(message = "cannot be empty")
+    private String name;
+
+    @NotNull(message = "cannot be null")
+    @NotBlank(message = "cannot be blank")
+    @NotEmpty(message = "cannot be empty")
+    private String room;
+
+    @NotNull(message = "cannot be null")
+    @NotEmpty(message = "cannot be empty")
+    private ClassStatus status;
+}
