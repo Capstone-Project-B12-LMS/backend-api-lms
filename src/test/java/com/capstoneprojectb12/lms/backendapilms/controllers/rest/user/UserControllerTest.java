291222a2ce5277788ba9c87a9f7d24583269cb06
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-06-10 01:07:43 +0700

    add test internal server error while login

--- src/test/java/com/capstoneprojectb12/lms/backendapilms/controllers/rest/user/UserControllerTest.java
+++ src/test/java/com/capstoneprojectb12/lms/backendapilms/controllers/rest/user/UserControllerTest.java
@@ -1,14 +1,21 @@
 package com.capstoneprojectb12.lms.backendapilms.controllers.rest.user;
 
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.util.NoSuchElementException;
+
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -16,20 +23,19 @@ import com.capstoneprojectb12.lms.backendapilms.controllers.rest.utils.Constant;
 import com.capstoneprojectb12.lms.backendapilms.controllers.rest.utils.JSON;
 import com.capstoneprojectb12.lms.backendapilms.models.dtos.user.UserLogin;
 import com.capstoneprojectb12.lms.backendapilms.models.dtos.user.UserNew;
+import com.capstoneprojectb12.lms.backendapilms.services.UserService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
 @TestMethodOrder(value = OrderAnnotation.class)
 @Tag(value = "userControllerTest")
 public class UserControllerTest {
         @Autowired
         private MockMvc mockMvc;
 
-        // @MockBean
-        // private UserService userService;
-
-        // @InjectMocks
-        // private UserController userController;
+        @MockBean
+        private UserService userService;
 
         private static final UserNew userNew = UserNew.builder()
                         .fullName("irda islakhu afa")
@@ -171,9 +177,9 @@ public class UserControllerTest {
         }
 
         @Test
-        @Disabled // TODO: How to mock service layer in controller layer
+        // @Disabled // TODO: How to mock service layer in controller layer
         public void testLoginInternalServerError() throws Exception {
-                // when(this.userService.findByEmail(anyString())).thenThrow(NoSuchElementException.class);
+                when(this.userService.findByEmail(anyString())).thenThrow(NoSuchElementException.class);
                 var user = UserLogin.builder()
                                 .email("myemail@gmail.com")
                                 .password("null")
@@ -187,5 +193,21 @@ public class UserControllerTest {
                                 .andExpect(status().isInternalServerError())
                 //
                 ;
+        }
+
+        @Test
+        @Disabled
+        public void testFindByIdUnauthorized() throws Exception {
+                var request = JSON.create(userNew);
+                var result = this.mockMvc.perform(post(Constant.BASE_URL + "/register")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON)
+                                .content(request))
+                                .andDo(print())
+                                .andExpect(status().isOk())
+                                .andReturn();
+
+                // result.getResponse
+                // this.mockMvc.perform(get(Constant.BASE_URL + "/users/", null))
         }
 }
