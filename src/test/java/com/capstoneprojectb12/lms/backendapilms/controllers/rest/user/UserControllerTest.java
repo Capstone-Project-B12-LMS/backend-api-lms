9e2492fcfcf3f4ed596c2a03240467cddba8536e
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-06-10 00:35:27 +0700

    modify something

--- src/test/java/com/capstoneprojectb12/lms/backendapilms/controllers/rest/user/UserControllerTest.java
+++ src/test/java/com/capstoneprojectb12/lms/backendapilms/controllers/rest/user/UserControllerTest.java
@@ -1,22 +1,14 @@
 package com.capstoneprojectb12.lms.backendapilms.controllers.rest.user;
 
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import java.util.NoSuchElementException;
-
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -24,22 +16,20 @@ import com.capstoneprojectb12.lms.backendapilms.controllers.rest.utils.Constant;
 import com.capstoneprojectb12.lms.backendapilms.controllers.rest.utils.JSON;
 import com.capstoneprojectb12.lms.backendapilms.models.dtos.user.UserLogin;
 import com.capstoneprojectb12.lms.backendapilms.models.dtos.user.UserNew;
-import com.capstoneprojectb12.lms.backendapilms.services.UserService;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ExtendWith(MockitoExtension.class)
 @TestMethodOrder(value = OrderAnnotation.class)
 @Tag(value = "userControllerTest")
 public class UserControllerTest {
         @Autowired
         private MockMvc mockMvc;
 
-        @MockBean
-        private UserService userService;
+        // @MockBean
+        // private UserService userService;
 
-        @InjectMocks
-        private UserController userController;
+        // @InjectMocks
+        // private UserController userController;
 
         private static final UserNew userNew = UserNew.builder()
                         .fullName("irda islakhu afa")
@@ -170,7 +160,7 @@ public class UserControllerTest {
         @Test
         @Disabled // TODO: How to mock service layer in controller layer
         public void testLoginInternalServerError() throws Exception {
-                when(this.userService.findByEmail(anyString())).thenThrow(NoSuchElementException.class);
+                // when(this.userService.findByEmail(anyString())).thenThrow(NoSuchElementException.class);
                 var user = UserLogin.builder()
                                 .email("myemail@gmail.com")
                                 .password("null")
