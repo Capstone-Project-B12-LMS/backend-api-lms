dcccd7b330505746dae603f0c18e3d7e7b0e522a
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-06-11 16:26:29 +0700

    test save new material in service layer

--- src/test/java/com/capstoneprojectb12/lms/backendapilms/services/MaterialServiceTest.java
+++ src/test/java/com/capstoneprojectb12/lms/backendapilms/services/MaterialServiceTest.java
@@ -1,40 +1,51 @@
 package com.capstoneprojectb12.lms.backendapilms.services;
 
-import com.capstoneprojectb12.lms.backendapilms.models.entities.Class;
+import com.capstoneprojectb12.lms.backendapilms.models.entities.Material;
 import com.capstoneprojectb12.lms.backendapilms.models.repositories.MaterialRepository;
-import org.junit.jupiter.api.Assertions;
+import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.capstoneprojectb12.lms.backendapilms.models.entities.Material;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 @Tag(value = "materialServiceTest")
 public class MaterialServiceTest {
+	
+	private final LocalDateTime deadline = LocalDateTime.now();
+	private final Material material = Material.builder().id("id").classes(ClassServiceTest.classEntity).title("material title").content("material content").topic(null).videoUri("url").fileUrl("url").deadline(deadline).point(100).category(null).build();
+	@MockBean
+	private MaterialRepository materialRepository;
+	@Autowired
+	private MaterialService materialService;
+	
+	@Test
+	public void testNotNull() {
+		assertNotNull(materialRepository);
+	}
+	
+	@Test
+	public void testSave() {
+//        success
+		when(this.materialRepository.save(any(Material.class))).thenReturn(material);
+		var result = this.materialService.save(material);
+		assertTrue(result.isPresent());
+		assertEquals(material.getContent(), result.get().getContent());
+		assertEquals(deadline, result.get().getDeadline());
 
-    @MockBean
-    private MaterialRepository materialRepository;
-
-    private final Material material = Material.builder()
-            .id("id")
-            .classes(ClassServiceTest.classEntity)
-            .title("material title")
-            .content("material content")
-            .topic(null)
-            .videoUri("url")
-            .fileUrl("url")
-            .point(100)
-            .category(null)
-            .build();
-
-    @Test
-    public void testNotNull(){
-        Assertions.assertNotNull(materialRepository);
-    }
-
+//        failed
+		when(this.materialRepository.save(any(Material.class))).thenReturn(null);
+		result = this.materialService.save(material);
+		assertFalse(result.isPresent());
+		assertThrows(NoSuchElementException.class, () -> this.materialService.save(material).get());
+	}
 }
