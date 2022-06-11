f72af512ec6c3af66ad422b109fbf6c2e7daa38e
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-06-11 18:09:52 +0700

    use Open API instead of springfox swagger

--- src/test/java/com/capstoneprojectb12/lms/backendapilms/controllers/SwaggerDocsTest.java
+++ src/test/java/com/capstoneprojectb12/lms/backendapilms/controllers/SwaggerDocsTest.java
@@ -1,5 +1,6 @@
 package com.capstoneprojectb12.lms.backendapilms.controllers;
 
+import com.capstoneprojectb12.lms.backendapilms.controllers.rest.utils.Constant;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,20 +11,18 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import com.capstoneprojectb12.lms.backendapilms.controllers.rest.utils.Constant;
-
 @SpringBootTest
 @Tag(value = "swaggerDocsTest")
 @AutoConfigureMockMvc
 public class SwaggerDocsTest {
-    @Autowired
-    private MockMvc mockMvc;
-
-    @Test
-    public void swaggerEndpointTest() throws Exception {
-        this.mockMvc.perform(MockMvcRequestBuilders.get(Constant.BASE_URL + "/docs/swagger-ui/index.html#/"))
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andReturn();
-    }
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Test
+	public void swaggerEndpointTest() throws Exception {
+		this.mockMvc.perform(MockMvcRequestBuilders.get(Constant.BASE_URL + "/swagger-ui/index.html"))
+				.andDo(MockMvcResultHandlers.print())
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andReturn();
+	}
 }
