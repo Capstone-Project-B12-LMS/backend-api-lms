36365cea347bc4f76ce5f52861a21f68148a5fd6
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-06-29 14:44:03 +0700

    create DTO to create new Guidance

--- src/main/java/com/capstoneprojectb12/lms/backendapilms/models/entities/Guidance.java
+++ src/main/java/com/capstoneprojectb12/lms/backendapilms/models/entities/Guidance.java
@@ -1,13 +1,14 @@
 package com.capstoneprojectb12.lms.backendapilms.models.entities;
 
-import javax.persistence.*;
-
+import com.capstoneprojectb12.lms.backendapilms.models.entities.utils.GuidanceStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.SQLDelete;
 
-import com.capstoneprojectb12.lms.backendapilms.models.entities.utils.GuidanceStatus;
-
-import lombok.*;
-import lombok.experimental.SuperBuilder;
+import javax.persistence.*;
 
 @Entity(name = "guidances")
 @Getter
@@ -17,17 +18,17 @@ import lombok.experimental.SuperBuilder;
 @SQLDelete(sql = "UPDATE guidances SET is_deleted = true WHERE id = ?")
 @SuperBuilder
 public class Guidance extends BaseEntity {
-
-    private String topic;
-
-    @Column(length = 1000, nullable = false)
-    private String content;
-
-    @ManyToOne
-    @JoinColumn(name = "class")
-    private Class classes;
-
-    @Enumerated(value = EnumType.STRING)
-    private GuidanceStatus status;
-
+	
+	private String topic;
+	
+	@Column(length = 1000, nullable = false)
+	private String content;
+	
+	@ManyToOne
+	@JoinColumn(name = "class")
+	private Class className;
+	
+	@Enumerated(value = EnumType.STRING)
+	private GuidanceStatus status;
+	
 }
--- src/main/java/com/capstoneprojectb12/lms/backendapilms/models/repositories/GuidanceRepository.java
+++ src/main/java/com/capstoneprojectb12/lms/backendapilms/models/repositories/GuidanceRepository.java
@@ -1,4 +1,9 @@
 package com.capstoneprojectb12.lms.backendapilms.models.repositories;
 
-public class GuidanceRepository {
+import com.capstoneprojectb12.lms.backendapilms.models.entities.Guidance;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GuidanceRepository extends JpaRepository<Guidance, String> {
 }
