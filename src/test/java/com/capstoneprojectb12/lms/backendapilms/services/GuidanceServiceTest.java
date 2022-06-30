3e8abf6146ae777956357b776b2bc682dc217796
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-06-30 09:20:59 +0700

    add unit test to save

--- src/test/java/com/capstoneprojectb12/lms/backendapilms/services/GuidanceServiceTest.java
+++ src/test/java/com/capstoneprojectb12/lms/backendapilms/services/GuidanceServiceTest.java
@@ -6,6 +6,7 @@ import com.capstoneprojectb12.lms.backendapilms.models.entities.utils.GuidanceSt
 import com.capstoneprojectb12.lms.backendapilms.models.repositories.ClassRepository;
 import com.capstoneprojectb12.lms.backendapilms.models.repositories.GuidanceRepository;
 import com.capstoneprojectb12.lms.backendapilms.models.repositories.UserRepository;
+import com.capstoneprojectb12.lms.backendapilms.utilities.exceptions.AnyException;
 import com.capstoneprojectb12.lms.backendapilms.utilities.exceptions.ClassNotFoundException;
 import com.capstoneprojectb12.lms.backendapilms.utilities.exceptions.UserNotFoundException;
 import java.util.Optional;
@@ -17,8 +18,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 
+import static com.capstoneprojectb12.lms.backendapilms.utilities.ApiResponse.extract;
+import static com.capstoneprojectb12.lms.backendapilms.utilities.ApiResponse.getResponse;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.when;
@@ -83,5 +88,67 @@ public class GuidanceServiceTest {
 	@Test
 	public void testSave() {
 //		success
+		when(this.userRepository.findById(anyString())).thenReturn(Optional.of(UserServiceTest.user));
+		when(this.classRepository.findById(anyString())).thenReturn(Optional.of(ClassServiceTest.classEntity));
+		when(this.guidanceRepository.save(any(Guidance.class))).thenReturn(guidance);
+		var res = this.guidanceService.save(guidanceNew);
+		var api = getResponse(res);
+		var data = extract(guidance, api);
+		assertNotNull(res);
+		assertEquals(HttpStatus.OK, res.getStatusCode());
+		assertNull(api.getErrors());
+		assertTrue(api.isStatus());
+		assertNotNull(data);
+		assertTrue(data.isPresent());
+		assertEquals(guidance.getId(), data.get().getId());
+		assertEquals(guidance.getStatus(), data.get().getStatus());
+		assertEquals(guidance.getClassEntity().getId(), data.get().getClassEntity().getId());
+		assertEquals(guidance.getUser().getEmail(), data.get().getUser().getEmail());
+		assertEquals(guidance.getClass(), data.get().getClass());
+		assertEquals(guidance.getContent(), data.get().getContent());
+		assertEquals(guidance.getTopic(), data.get().getTopic());
+		reset(this.classRepository, this.guidanceRepository, this.userRepository);
+
+//		user not found
+		when(this.userRepository.findById(anyString())).thenReturn(Optional.empty());
+		when(this.classRepository.findById(anyString())).thenReturn(Optional.of(ClassServiceTest.classEntity));
+		when(this.guidanceRepository.save(any(Guidance.class))).thenReturn(guidance);
+		res = this.guidanceService.save(guidanceNew);
+		api = getResponse(res);
+		data = extract(guidance, api);
+		assertNotNull(res);
+		assertEquals(HttpStatus.BAD_REQUEST, res.getStatusCode());
+		assertNotNull(api.getErrors());
+		assertFalse(api.isStatus());
+		assertTrue(data.isEmpty());
+		reset(this.classRepository, this.guidanceRepository, this.userRepository);
+
+//		class not found
+		when(this.userRepository.findById(anyString())).thenReturn(Optional.of(UserServiceTest.user));
+		when(this.classRepository.findById(anyString())).thenReturn(Optional.empty());
+		when(this.guidanceRepository.save(any(Guidance.class))).thenReturn(guidance);
+		res = this.guidanceService.save(guidanceNew);
+		api = getResponse(res);
+		data = extract(guidance, api);
+		assertNotNull(res);
+		assertEquals(HttpStatus.BAD_REQUEST, res.getStatusCode());
+		assertNotNull(api.getErrors());
+		assertFalse(api.isStatus());
+		assertTrue(data.isEmpty());
+		reset(this.classRepository, this.guidanceRepository, this.userRepository);
+
+//		any exception
+		when(this.userRepository.findById(anyString())).thenReturn(Optional.of(UserServiceTest.user));
+		when(this.classRepository.findById(anyString())).thenReturn(Optional.of(ClassServiceTest.classEntity));
+		when(this.guidanceRepository.save(any(Guidance.class))).thenThrow(AnyException.class);
+		res = this.guidanceService.save(guidanceNew);
+		api = getResponse(res);
+		data = extract(guidance, api);
+		assertNotNull(res);
+		assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, res.getStatusCode());
+		assertNotNull(api.getErrors());
+		assertFalse(api.isStatus());
+		assertTrue(data.isEmpty());
+		reset(this.classRepository, this.guidanceRepository, this.userRepository);
 	}
 }
