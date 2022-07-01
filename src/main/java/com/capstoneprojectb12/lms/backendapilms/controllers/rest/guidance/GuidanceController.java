fd1debebda8684bed33c66c2b2a5b9a75ed97421
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-07-01 17:23:54 +0700

    route method find by class id

--- src/main/java/com/capstoneprojectb12/lms/backendapilms/controllers/rest/guidance/GuidanceController.java
+++ src/main/java/com/capstoneprojectb12/lms/backendapilms/controllers/rest/guidance/GuidanceController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -28,5 +25,10 @@ public class GuidanceController {
 			return errorValidation(errors);
 		}
 		return this.guidanceService.save(request);
+	}
+	
+	@GetMapping(value = {"/class/{classId}"})
+	public ResponseEntity<?> findAllByClassId(@PathVariable(required = true, name = "classId") String classId) {
+		return this.guidanceService.findAllByClassId(classId);
 	}
 }
