a23ff68f382b84e04d3825c3166bf2f769279436
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-07-02 11:09:42 +0700

    add feature to get feedback by class id

--- src/main/java/com/capstoneprojectb12/lms/backendapilms/controllers/rest/feedback/FeedbackController.java
+++ src/main/java/com/capstoneprojectb12/lms/backendapilms/controllers/rest/feedback/FeedbackController.java
@@ -6,9 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -17,6 +15,7 @@ import static com.capstoneprojectb12.lms.backendapilms.utilities.ApiResponse.err
 @Slf4j
 @RequiredArgsConstructor
 @RestController
+@RequestMapping(value = {"/restapi/v1/feedbacks"})
 public class FeedbackController {
 	private final FeedbackService feedbackService;
 	
@@ -26,5 +25,10 @@ public class FeedbackController {
 			return errorValidation(errors);
 		}
 		return this.feedbackService.save(request);
+	}
+	
+	@GetMapping(value = {"/class/{classId}"})
+	public ResponseEntity<?> findAllByClassId(@PathVariable(name = "classId", required = true) String classId) {
+		return this.feedbackService.findAllByClassId(classId);
 	}
 }
