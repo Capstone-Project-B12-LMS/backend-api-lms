d62d3fb301d425bdeb99e4311e0ee4dcb811f2fe
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-07-04 12:57:25 +0700

    add graphql mutation to delete guidance by id

--- src/main/java/com/capstoneprojectb12/lms/backendapilms/controllers/gql/guidance/GuidanceMutation.java
+++ src/main/java/com/capstoneprojectb12/lms/backendapilms/controllers/gql/guidance/GuidanceMutation.java
@@ -12,28 +12,37 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
+import static com.capstoneprojectb12.lms.backendapilms.models.dtos.base.ResponseDelete.deleted;
+import static com.capstoneprojectb12.lms.backendapilms.models.dtos.base.ResponseDelete.notDeleted;
 import static com.capstoneprojectb12.lms.backendapilms.utilities.ApiResponse.extract;
+import static com.capstoneprojectb12.lms.backendapilms.utilities.ApiResponse.getResponse;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
 @SchemaMapping(typeName = "GuidanceMutation")
 public class GuidanceMutation implements BaseMutation<Guidance, GuidanceNew, GuidanceUpdate> {
-	private final GuidanceService guidanceService;
-	
-	@Override
-	@SchemaMapping(field = "save")
-	public Guidance save(@Argument(name = "request") GuidanceNew request) {
-		return extract(new Guidance(), this.guidanceService.save(request)).orElse(null);
-	}
-	
-	@Override
-	public Guidance update(String id, GuidanceUpdate request) {
-		return null;
-	}
-	
-	@Override
-	public ResponseDelete<Guidance> deleteById(String id) {
-		return null;
-	}
+    private final GuidanceService guidanceService;
+
+    @Override
+    @SchemaMapping(field = "save")
+    public Guidance save(@Argument(name = "request") GuidanceNew request) {
+        return extract(new Guidance(), this.guidanceService.save(request)).orElse(null);
+    }
+
+    @Override
+    public Guidance update(String id, GuidanceUpdate request) {
+        return null;
+    }
+
+    @Override
+    @SchemaMapping(field = "deleteById")
+    public ResponseDelete<Guidance> deleteById(@Argument(name = "id") String id) {
+        var res = this.guidanceService.deleteById(id);
+        var guidance = extract(new Guidance(), res);
+        if (guidance.isPresent()) {
+            return deleted(guidance.get());
+        }
+        return notDeleted(getResponse(res).getErrors());
+    }
 }
