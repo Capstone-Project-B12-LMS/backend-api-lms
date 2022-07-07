2c64a7797cd1cb3a87525339d7f439d3ca90b2ae
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-07-07 15:37:58 +0700

    save history when save, update, deleteById and deleteUserById on Class

---
+++ src/main/java/com/capstoneprojectb12/lms/backendapilms/utilities/histories/ActivityHistoryUtils.java
@@ -0,0 +1,7 @@
+package com.capstoneprojectb12.lms.backendapilms.utilities.histories;
+
+public class ActivityHistoryUtils {
+	public static String youAreSuccessfully(String text) {
+		return "You have successfully " + text.trim();
+	}
+}
