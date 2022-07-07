f40cc01727124ae97f7468edf14b7be504bbb5eb
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-07-08 01:16:57 +0700

    add authorization in graphql controllers

--- src/main/java/com/capstoneprojectb12/lms/backendapilms/controllers/gql/GraphQLMutation.java
+++ src/main/java/com/capstoneprojectb12/lms/backendapilms/controllers/gql/GraphQLMutation.java
@@ -1,5 +1,9 @@
 package com.capstoneprojectb12.lms.backendapilms.controllers.gql;
 
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+
 import com.capstoneprojectb12.lms.backendapilms.controllers.gql.classes.ClassMutation;
 import com.capstoneprojectb12.lms.backendapilms.controllers.gql.feedback.FeedbackMutation;
 import com.capstoneprojectb12.lms.backendapilms.controllers.gql.guidance.GuidanceMutation;
@@ -7,14 +11,13 @@ import com.capstoneprojectb12.lms.backendapilms.controllers.gql.hello.HelloMutat
 import com.capstoneprojectb12.lms.backendapilms.controllers.gql.material.MaterialMutation;
 import com.capstoneprojectb12.lms.backendapilms.controllers.gql.role.RoleMutation;
 import com.capstoneprojectb12.lms.backendapilms.controllers.gql.user.UserMutation;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.graphql.data.method.annotation.SchemaMapping;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.stereotype.Controller;
 
 @Controller
 @SchemaMapping(typeName = "Mutation")
 @RequiredArgsConstructor
+@PreAuthorize(value = "hasAnyAuthority('USER')")
 public class GraphQLMutation {
 	private final HelloMutation helloMutation;
 	private final RoleMutation roleMutation;
@@ -23,41 +26,41 @@ public class GraphQLMutation {
 	private final MaterialMutation materialMutation;
 	private final GuidanceMutation guidanceMutation;
 	private final FeedbackMutation feedbackMutation;
-	
+
 	@SchemaMapping(field = "hello")
 	public HelloMutation helloMutation() {
 		return this.helloMutation;
 	}
-	
-	//	@PreAuthorize(value = "hasAnyAuthority('USER')") // TODO: enable security
+
+	// @PreAuthorize(value = "hasAnyAuthority('USER')") // TODO: enable security
 	@SchemaMapping(field = "role")
 	public RoleMutation roleMutation() {
 		return this.roleMutation;
 	}
-	
-	//	@PreAuthorize(value = "hasAnyAuthority('USER')") // TODO: enable security
+
+	// @PreAuthorize(value = "hasAnyAuthority('USER')") // TODO: enable security
 	@SchemaMapping(field = "user")
 	public UserMutation userMutation() {
 		return this.userMutation;
 	}
-	
+
 	@PreAuthorize(value = "hasAnyAuthority('USER')") // TODO: enable security
 	@SchemaMapping(field = "class")
 	public ClassMutation classMutation() {
 		return this.classMutation;
 	}
-	
-	//	@PreAuthorize(value = "hasAnyAuthority('USER')") // TODO: enable security
+
+	// @PreAuthorize(value = "hasAnyAuthority('USER')") // TODO: enable security
 	@SchemaMapping(field = "material")
 	public MaterialMutation materialMutation() {
 		return this.materialMutation;
 	}
-	
+
 	@SchemaMapping(field = "guidance")
 	public GuidanceMutation guidanceMutation() {
 		return this.guidanceMutation;
 	}
-	
+
 	@SchemaMapping(field = "feedback")
 	public FeedbackMutation feedbackMutation() {
 		return this.feedbackMutation;
