1f1e6613a36a0ec9569f93c347daff60bf501dc6
Irda Islakhu Afa <65246900+irdaislakhuafa@users.noreply.github.com>
2023-04-10 01:25:24 +0700

    change name to uppercase (#61)
    * change name to uppercase
    
    * fix binding
    
    * fix template html
    
    * temporary remove cc
    
    * delete unused file
    
    * add env

--- src/main/java/com/capstoneprojectb12/lms/backendapilms/services/EmailService.java
+++ src/main/java/com/capstoneprojectb12/lms/backendapilms/services/EmailService.java
@@ -36,9 +36,9 @@ public class EmailService {
 					.from(new MailAddress(myEmail, myName))
 					.to(new MailAddress(to, name))
 					.subject("Email Verification")
-					.param("name", name)
+					.param("name", name.toUpperCase())
 					.param("verify", ServletUriComponentsBuilder.fromContextPath(request).toUriString() + String.format("/restapi/v1/users/verify/%s/%s", user.getId(), this.jwtUtils.generateTokenString(user)))
-					.loadTemplate("email/verify.html")
+					.loadTemplate("verify.html")
 					.build();
 			return mailProvider.send(mail);
 		} catch (Exception e) {
